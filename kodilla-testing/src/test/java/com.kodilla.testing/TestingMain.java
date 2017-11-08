490f10920071f884aef5d681e96d7036266d02b3
Tomek <tomek371240@gmail.com>
2017-11-08 22:21:46 +0100

    kodilla-testing

---
+++ kodilla-testing/src/test/java/com.kodilla.testing/TestingMain.java
@@ -0,0 +1,33 @@
+package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class TestingMain {
+    public static void main(String[] args){
+//        SimpleUser simpleUser = new SimpleUser("theForumUser");
+//
+//        String result = simpleUser.getUsername();
+//
+//        if (result.equals("theForumUser")){
+//            System.out.println("test OK");
+//        } else {
+//            System.out.println("Error!");
+//        }
+//        Calculator calculator = new Calculator();
+//        int add =calculator.add(1,2);
+//        if(add == 3){
+//            System.out.println("Function Add works");
+//        }
+//        else {
+//            System.out.println("Function Add NOT works");
+//        }
+//        int sub = calculator.subtract(4,3);
+//        if (sub == 1) {
+//            System.out.println("Function substract work");
+//        }
+//        else {
+//            System.out.println("Function substract NOT work");
+//        }
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,61 @@
+package com.kodilla.testing.collection;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+public class CollectionTestSuite {
+
+    @Before
+    public void before() {
+        System.out.println("Test Case: begin ");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test Suite before");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test Suite end");
+    }
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        ArrayList<Integer> t = new ArrayList<>();
+        t.add(1);
+        t.add(2);
+        t.add(3);
+        t.add(4);
+        t.add(5);
+        t.add(6);
+
+        ArrayList<Integer> t2 = new ArrayList<>();
+                oddNumbersExterminator.exterminate(t);
+
+    for(int i : t2){
+        System.out.println(t2.get(i));
+    }
+    ArrayList<Integer> t3 = new ArrayList<>();
+    t3.add(2);
+    t3.add(4);
+    t3.add(6);
+
+    Assert.assertArrayEquals(t3.toArray(),t2.toArray());
+
+    }
+
+
+}
+
+
+//    testOddNumbersExterminatorEmptyList (sprawdzi czy klasa zachowuje się poprawnie gdy lista jest pusta)
+//    testOddNumbersExterminatorNormalList (sprawdzi czy klasa zachowuje się poprawnie gdy lista zawiera liczby parzyste i nieparzyste)
