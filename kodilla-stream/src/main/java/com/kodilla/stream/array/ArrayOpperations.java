07655d116d70fb0b2656ec9ddeeaa88b9eda33d3
tomek401273 <tomek371240@gmail.com>
2017-11-18 09:11:00 +0100

    exercise5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOpperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOpperations.java
@@ -1,19 +1,22 @@
 package com.kodilla.stream.array;
 
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOpperations {
-    static double getAverage(int[] numbers) {
+    static OptionalDouble getAverage(int[] numbers) {
 
 
         System.out.println("lenhth numbers: " + numbers.length);
 
-        double average = IntStream.range(1, numbers.length + 1)
-                .sum() / numbers.length;
+        OptionalDouble average = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average();
         //jak tutaj użyję average to nażuca mi zamiast double OptionalDouble
 
 
-        IntStream.range(1, numbers.length + 1)
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
                 .forEach(System.out::println);
 
         System.out.println("Average numbers: " + average);
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -3,6 +3,8 @@ package com.kodilla.stream.array;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.OptionalDouble;
+
 public class ArrayOperationsTestSuite {
 
     @Test
@@ -16,11 +18,29 @@ public class ArrayOperationsTestSuite {
         tab[4] = 5;
 
         //When
-        double calculatedAverage = ArrayOpperations.getAverage(tab);
-        double predicedAverage = 3.0;
+        OptionalDouble calzculatedAverage = ArrayOpperations.getAverage(tab);
+        OptionalDouble predicedAverage = OptionalDouble.of(3.0);
 
         //Then
-        Assert.assertEquals(predicedAverage, calculatedAverage,0.1);
+
+        Assert.assertEquals(predicedAverage, calzculatedAverage);
     }
 
+    @Test
+    public void ArrayOperationsTestSuite2(){
+        //Given
+        int [] tab = new int[5];
+        tab[0] = 1;
+        tab[1] = 1;
+        tab[2] = 1;
+        tab[3] = 1;
+        tab[4] = 1;
+
+        //When
+        OptionalDouble calculatedAverage = ArrayOpperations.getAverage(tab);
+        OptionalDouble predicedAverage = OptionalDouble.of(1.0);
+
+        //Then
+        Assert.assertEquals(predicedAverage, calculatedAverage);
+    }
 }
