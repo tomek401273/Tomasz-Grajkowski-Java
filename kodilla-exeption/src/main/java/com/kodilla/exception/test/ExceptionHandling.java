60b19f368689efc638d26e73c6120a63d01affdc
tomek401273 <tomek371240@gmail.com>
2017-11-22 23:10:13 +0100

    exercise8.3

--- kodilla-exeption/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exeption/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -2,4 +2,30 @@ package com.kodilla.exception.test;
 
 public class ExceptionHandling {
 
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+          String proably = secondChallenge.probablyIWillThrowException(1,2);
+            System.out.println(proably);
+        } catch (Exception e) {
+            System.out.println("We have error: "+e);
+            System.out.println("Something go wrong, Check inserted numers and try again");
+        }finally {
+            System.out.println("Check1");
+
+        }
+        System.out.println();
+
+        try {
+           String proably = secondChallenge.probablyIWillThrowException(1,1.5);
+            System.out.println(proably);
+        } catch (Exception e) {
+            System.out.println("We have error: "+e);
+            System.out.println("Something go wrong, Check inserted numers and try again");
+        } finally {
+            System.out.println("Check2");
+
+        }
+
+    }
 }
