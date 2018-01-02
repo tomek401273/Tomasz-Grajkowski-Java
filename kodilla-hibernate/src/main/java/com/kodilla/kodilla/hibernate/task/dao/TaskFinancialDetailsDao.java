c6dfd13bc46ea389794a7cd4a805a3945013933a
tomek401273 <tomek371240@gmail.com>
2018-01-02 20:45:35 +0100

    problem

---
+++ kodilla-hibernate/src/main/java/com/kodilla/kodilla/hibernate/task/dao/TaskFinancialDetailsDao.java
@@ -0,0 +1,6 @@
+package com.kodilla.kodilla.hibernate.task.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TaskFinancialDetailsDao extends CrudRepository {
+}
--- kodilla-hibernate/src/test/java/com/kodilla/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -34,4 +34,5 @@ public class TaskListDaoTestSuite {
         //CleanUp
         taskListDao.deleteAll();
     }
+
 }
