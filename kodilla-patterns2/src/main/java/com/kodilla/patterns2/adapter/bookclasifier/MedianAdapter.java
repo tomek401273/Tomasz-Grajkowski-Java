62fb98c7fe161e5408c6be8ba20958e97615df0e
tomek401273 <tomek371240@gmail.com>
2018-03-05 20:28:50 +0100

    addapter-map-to-colllect

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -8,27 +8,25 @@ import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
     @Override
     public int publicationYearMedian(Set<BookA> bookSet) {
-        Map<BookSignature, BookB> bookMap = new HashMap<>();
-        for (BookA bookA: bookSet) {
-            BookB bookB = new BookB(bookA.getAuthor(),bookA.getTitle(),bookA.getPublicationYear());
-            BookSignature bookSignature = new BookSignature(bookA.getSignature());
-            bookMap.put(bookSignature,bookB);
-        }
-      return medianPublicationYear(bookMap);
+        Map<BookSignature, BookB> bookMap = bookSet.stream()
+                .collect(Collectors.toMap(
+                        bookA -> new BookSignature(bookA.getSignature()),
+                        bookA -> new BookB(bookA.getAuthor(), bookA.getTitle(), bookA.getPublicationYear())));
+        return medianPublicationYear(bookMap);
     }
 
     @Override
     public int publicationYearAverage(Set<BookA> bookSet) {
-        Map<BookSignature, BookB> bookMap = new HashMap<>();
-        for (BookA bookA: bookSet) {
-            BookB bookB = new BookB(bookA.getAuthor(),bookA.getTitle(),bookA.getPublicationYear());
-            BookSignature bookSignature = new BookSignature(bookA.getSignature());
-            bookMap.put(bookSignature, bookB);
-        }
+        Map<BookSignature, BookB> bookMap = bookSet.stream()
+                .collect(Collectors.toMap(
+                        bookA -> new BookSignature(bookA.getSignature()),
+                        bookA -> new BookB(bookA.getAuthor(), bookA.getTitle(), bookA.getPublicationYear())
+                ));
         return averagePublicationYear(bookMap);
     }
 }
